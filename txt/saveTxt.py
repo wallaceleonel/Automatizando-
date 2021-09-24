c18131bf812ddbc57ce97cfb594d6a3af807256f
Wallace Leonel <69112012+wallaceleonel@users.noreply.github.com>
2021-09-24 00:24:53 -0300

    criando um vim?

---
+++ txt/saveTxt.py
@@ -0,0 +1,33 @@
+# MODIFICANDO ARQUIVOS 
+valores_celulares [1400,2500,3000,8000,12000]
+
+'''
+
+'r'  -> Usado somente para ler  algo
+'w'  -> Usado somente para escrever algo
+'r+' -> Usado para ler e escrever algo 
+'a'  -> Usado para escrever algo 
+
+'''
+#Criar menu de funcionalidades?
+ 
+#with open ('valores_celulares.txt','w') as arquivo:
+ #   for valor in valores_celulares:
+  #      arquivo.write(str(valor) + '\n')
+
+
+#with open ('valores_celulares.txt','a') as arquivo:
+ #   for valor in valores_celulares:
+  #      arquivo.write(str(valor) + '\n')
+
+#with open ('valores_celulares.txt','r') as arquivo:
+ #   for valor in arquivo:
+  #      print(valor)
+
+# with open ('valores_celulares.txt','r+') as arquivo:
+ #   for valor in arquivo:
+  #      print(valor))
+   # arquivo.write("")
+        
+
+                
